897f3ca7206c426702c5f391889a82033f253eaa
jholman <jholman@jholmans-mbp.attlocal.net>
2018-02-17 17:16:10 -0800

    block and chain impl v1

--- block.js
+++ block.js
@@ -1,13 +1,13 @@
-const SHA256 = require('crypto-js/sha256');
+const SHA256 = require('crypto-js/sha256'),
 
-const Block = (index, timestamp, data, previousHash) => {
-  const calculateHash = () => SHA256(index + timestamp + JSON.stringify(data) + previousHash);
+  calculateHash = (index, timestamp, data, previousHash) => SHA256(index + timestamp + JSON.stringify(data) + previousHash);
 
-  return ({
-    index,
-    previousHash,
-    timestamp,
-    data,
-    hash: calculateHash()
-  })
-}
+  Block = (index, timestamp, data, previousHash) => {
+    return ({
+      index,
+      previousHash,
+      timestamp,
+      data,
+      hash: calculateHash(index, timestamp, data, previousHash)
+    })
+  }
